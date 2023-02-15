f21a55995d4ab85567cb9e226b8def9af2fbbc25
Vasilii <pur1990@yandex.ru>
2023-02-16 01:28:09 +0300

    Задача 2

---
+++ Lesson1-2/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine();
+string[] Days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+System.Console.Write("Введите номер дня недели:   ");
+string writeDay = Console.ReadLine();
+int dayNumber = Convert.ToInt32(writeDay);
+
+System.Console.WriteLine();
+if (0 < dayNumber && dayNumber < 8) System.Console.WriteLine($"Это - {Days[dayNumber - 1]}");
+else System.Console.WriteLine("Дня недели с таким номером не существует");
+System.Console.WriteLine();
