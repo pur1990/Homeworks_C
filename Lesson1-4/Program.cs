76e60aaa66ba9e21b8fee8d0bba1dfd83c948063
Vasilii <pur1990@yandex.ru>
2023-02-16 01:56:27 +0300

    Задача 4 решена

---
+++ Lesson1-4/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Выяснить является ли число чётным");
+System.Console.WriteLine();
+System.Console.Write("Введите число: ");
+string writeNumber = Console.ReadLine();
+int number = Convert.ToInt32(writeNumber);
+string result = "Введённое Вами число не является чётным";
+if (number % 2 == 0)
+{
+    result = result.Replace(" не", "");
+}
+System.Console.WriteLine(result);
