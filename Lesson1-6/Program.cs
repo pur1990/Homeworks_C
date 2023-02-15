1327a6534ce644e0a87fa2815fdedacca0429fc3
Vasilii <pur1990@yandex.ru>
2023-02-16 02:25:16 +0300

    Решена задача 6

---
+++ Lesson1-6/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Показать последнюю цифру трёхзначного числа");
+System.Console.WriteLine();
+System.Console.WriteLine("Введите трёхзначное число: ");
+string number = Console.ReadLine();
+int num = Convert.ToInt32(number);
+if (num / 100 >= 10 && num / 100 < 1)
+{
+    Console.Write("Вы ввели не трехзначное число!");
+}
+else
+{
+    Console.Write($"Последняя цифра введённого Вами числа - {num % 100 % 10}");
+}
