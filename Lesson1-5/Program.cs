385490b9fa71f2ed82214021b954b0b02094255d
Vasilii <pur1990@yandex.ru>
2023-02-16 02:03:31 +0300

    Задача 5 решена

---
+++ Lesson1-5/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Показать четные числа от 1 до N");
+System.Console.WriteLine();
+System.Console.Write("Введите число: ");
+string writeN = Console.ReadLine();
+int N = Convert.ToInt32(writeN);
+int num = 0;
+System.Console.WriteLine($"Чётные числа из диапазона от 1 до {N}:");
+while (num < N)
+{
+    num++;
+    if (num % 2 == 0) System.Console.Write($"{num} ");
+}
+System.Console.WriteLine();
