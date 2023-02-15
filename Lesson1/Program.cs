350e1e78b7e691a5c42b9bf1e498f0bf427ff378
Vasilii <pur1990@yandex.ru>
2023-02-16 01:43:43 +0300

    Добавлено описание задачи 1

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,4 +1,5 @@
-﻿System.Console.WriteLine("Задача 1:");
+﻿System.Console.WriteLine("По двум заданным числам проверять является ли первое квадратом второго");
+System.Console.WriteLine();
 System.Console.WriteLine("Введите первое число: ");
 string Number1 = Console.ReadLine();
 int num1 = Convert.ToInt32(Number1);
