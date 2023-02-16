eb5a9e86f699cdebb4a86a57b295cde090fe742d
Vasilii <pur1990@yandex.ru>
2023-02-16 03:20:05 +0300

    Решение задачи 10

---
+++ Lesson1-10/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Выяснить, кратно ли число заданному, если нет, вывести остаток.");
+System.Console.WriteLine();
+int num1 = new Random().Next(11, 101);
+int num2 = new Random().Next(1, 11);
+Console.WriteLine($"Были выбраны случайным образом числа {num1} и {num2}");
+int result = num1 % num2;
+if (result == 0)
+    System.Console.WriteLine($"Число {num1} кратно числу {num2}");
+else
+    Console.WriteLine($"Число {num1} не кратно числу {num2}, остаток от деления равен {num1 % num2}");
