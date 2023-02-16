a4624d447b27feb20820972da55efd13b2997db0
Vasilii <pur1990@yandex.ru>
2023-02-16 03:25:49 +0300

    Решение задачи 11

---
+++ Lesson1-11/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Найти третью цифру числа или сообщить, что её нет");
+System.Console.WriteLine();
+System.Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int num = Convert.ToInt32(number);
+if (num / 100 < 1)
+    Console.Write("Третьей цифры числа нет!");
+else
+    Console.Write($"Третья цифра введённого Вами числа - {num / 100 % 10}");
