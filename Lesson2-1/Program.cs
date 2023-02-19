753e6c846896be8075b3d78c253c3ab4c5719a27
Vasilii <pur1990@yandex.ru>
2023-02-19 23:34:12 +0300

    Решение 2 урока 1 задачи

---
+++ Lesson2-1/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Дано число обозначающее день недели. Выяснить является номер дня недели выходным");
+System.Console.WriteLine();
+string[] Days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+System.Console.Write("Введите номер дня недели: ");
+string writeDay = Console.ReadLine();
+int dayNumber = Convert.ToInt32(writeDay);
+System.Console.WriteLine();
+if (0 < dayNumber && dayNumber < 5)
+    System.Console.WriteLine($"Это будет {Days[dayNumber - 1]} - это будний день!");
+else if (dayNumber == 6 || dayNumber == 7)
+    System.Console.WriteLine($"Ура! Это {Days[dayNumber - 1]}, и это выходной день!");
+else
+    System.Console.WriteLine("Дня недели с таким номером не существует!");
