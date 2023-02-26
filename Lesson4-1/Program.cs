ec9dbcf5d32954d88760b3a6111a022163dce608
Vasilii <pur1990@yandex.ru>
2023-02-26 23:00:42 +0300

    Решение задачи 4-1

---
+++ Lesson4-1/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран");
+System.Console.WriteLine();
+int n = 8;
+int[] arr = new int[n];
+Random rand = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = rand.Next(0,2);
+}
+Console.WriteLine(string.Join(", ", arr));
