a68b8f73e2408092d06e450e27b1b6cffb81fe03
Vasilii <pur1990@yandex.ru>
2023-03-02 01:48:34 +0300

    Добавлены решения задачи 1 и 3

---
+++ Lesson5-1/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы");
+System.Console.WriteLine();
+System.Console.Write("Введите k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+/// k1 * x + b1 = k2 * x + b2
+/// (k1 - k2) * x = b2 - b1
+var x = (b2 - b1) / (k1 - k2);
+var y = k1 * x + b1;
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+System.Console.WriteLine($"Пересечение прямых в точке: ({x};{y})");
---
+++ Lesson5-3/Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.WriteLine("Написать программу копирования массива");
+System.Console.WriteLine();
+int[] arr = {1, 2, 3, 4, 5};
+int[] copy = new int[arr.Length];
+arr.CopyTo(copy, 0);
+System.Console.WriteLine(String.Join(", ", copy));
