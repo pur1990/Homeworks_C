45ba77e7736da087a8d462bb6b048993b77164cc
Vasilii <pur1990@yandex.ru>
2023-03-06 00:03:23 +0300

    Решение ДЗ к 8 уроку

---
+++ Lesson8-1/Program.cs
@@ -0,0 +1,69 @@
+﻿System.Console.WriteLine("Найти произведение двух матриц");
+System.Console.WriteLine();
+int m = InputNumbers("Введите число строк 1-й матрицы: ");
+int n = InputNumbers("Введите число столбцов 1-й матрицы: ");
+int o = InputNumbers("Введите число строк 2-й матрицы: ");
+int p = InputNumbers("Введите число столбцов 2-й матрицы: ");
+int range = InputNumbers("Введите диапазон случайных чисел: от 1 до ");
+
+int[,] firstMx = new int[m, n];
+CreateArray(firstMx);
+System.Console.WriteLine($"Первая матрица:");
+WriteArray(firstMx);
+
+int[,] secondMx = new int[o, p];
+CreateArray(secondMx);
+Console.WriteLine($"Вторая матрица:");
+WriteArray(secondMx);
+
+int[,] resultMx = new int[m, p];
+
+MultiplyMatrix(firstMx, secondMx, resultMx);
+Console.WriteLine($"Произведение первой и второй матриц:");
+WriteArray(resultMx);
+
+void MultiplyMatrix(int[,] firstMx, int[,] secondMx, int[,] resultMx)
+{
+    for (int i = 0; i < resultMx.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMx.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < firstMx.GetLength(1); k++)
+            {
+                sum += firstMx[i, k] * secondMx[k, j];
+            }
+            resultMx[i, j] = sum;
+        }
+    }
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Lesson8-2/Program.cs
@@ -0,0 +1,89 @@
+﻿System.Console.WriteLine("В двумерном массиве целых чисел. Удалить строку и столбец, на пересечении которых расположен наименьший элемент.");
+System.Console.WriteLine();
+int m = InputNumbers("Введите количество строк: ");
+int n = InputNumbers("Введите количество строк: ");
+int range = InputNumbers("Введите диапазон: от 1 до ");
+
+int[,] array = new int[m, n];
+CreateArray(array);
+WriteArray(array);
+
+int[,] position = new int[1, 2];
+position = FindPosition(array, position);
+System.Console.Write($"Позиция элемента:");
+WriteArray(position);
+
+int[,] arr = new int[array.GetLength(0) - 1, array.GetLength(1) - 1];
+DeleteLines(array, position, arr);
+System.Console.WriteLine("Получившийся массив:");
+WriteArray(arr);
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void CreateArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FindPosition(int[,] array, int[,] position)
+{
+    int temp = array[0, 0];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] <= temp)
+            {
+                position[0, 0] = i;
+                position[0, 1] = j;
+                temp = array[i, j];
+            }
+        }
+    }
+    Console.WriteLine($"Mинимальный элемент: {temp}");
+    return position;
+}
+
+void DeleteLines(int[,] array, int[,] positionSmallElement, int[,] arrayWithoutLines)
+{
+    int k = 0, l = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (positionSmallElement[0, 0] != i && positionSmallElement[0, 1] != j)
+            {
+                arrayWithoutLines[k, l] = array[i, j];
+                l++;
+            }
+        }
+        l = 0;
+        if (positionSmallElement[0, 0] != i)
+        {
+            k++;
+        }
+    }
+}
