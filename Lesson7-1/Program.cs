5c0ada67906a6f8e2448ec34c77e6da64ba5ce15
Vasilii <pur1990@yandex.ru>
2023-03-05 23:32:03 +0300

    Решение задач к 7 уроку

---
+++ Lesson7-1/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine("Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.");
+System.Console.WriteLine();
+System.Console.WriteLine("Введите количество строк:");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов:");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] arr = new int[n, m];
+Random ran = new Random();
+int sum = 0;
+float middle = 0;
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        arr[i, j] = ran.Next(0, 100);
+        sum += arr[i, j];
+        middle = sum / (m * n);
+        Console.Write(arr[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+System.Console.WriteLine("Среднее арифметическое каждого из столбцов = " + middle);
---
+++ Lesson7-3/Program.cs
@@ -0,0 +1,56 @@
+﻿System.Console.WriteLine("В прямоугольной матрице найти строку с наименьшей суммой элементов.");
+System.Console.WriteLine();
+int m = InputNumbers("Введите количество строк: ");
+int n = InputNumbers("Введите количество столбцов: ");
+int range = InputNumbers("Введите диапазон: от 1 до ");
+int[,] array = new int[m, n];
+NewArray(array);
+WriteArray(array);
+int sumLine = 0;
+int sum = SumLineElements(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+    int tempSumLine = SumLineElements(array, i);
+    if (sum > tempSumLine)
+    {
+        sum = tempSumLine;
+        sumLine = i;
+    }
+}
+System.Console.WriteLine($"\n{sumLine + 1} - строкa с наименьшей суммой элементов = {sum}");
+int SumLineElements(int[,] array, int i)
+{
+    int sumLine = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sumLine += array[i, j];
+    }
+    return sumLine;
+}
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+void NewArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(range);
+        }
+    }
+}
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
